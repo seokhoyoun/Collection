55564e6fd105642794459c63cd331401893217f9
Seokho Youn <axlover@hotmail.com>
2018-12-21 20:19:45 +0900

    게시판 재구현중

---
+++ src/rewind/BoardManager.java
@@ -0,0 +1,95 @@
+package rewind;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+import board.model.vo.Board;
+
+public class BoardManager {
+	// Field
+	ArrayList<Board> list = new ArrayList<Board>();
+	Scanner sc = new Scanner(System.in);
+	
+	public BoardManager() {
+		try(ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream("board_list.dat")))){
+			while(true) {
+				list.add((Board) ois.readObject());
+			}
+		} catch (EOFException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void writeBoard() {
+		GregorianCalendar now = new GregorianCalendar();
+		StringBuilder sb = new StringBuilder();
+		int count = 0;
+		System.out.println("새 게시글을 작성합니다.");
+		System.out.println("글 제목 : ");
+		String title = sc.nextLine();
+		System.out.println("작성자 : ");
+		String name = sc.next();
+		System.out.println("글 내용 : ");
+		String line = null;
+		while(!(line = sc.nextLine()).equals("exit")) {
+			sb.append(line).append("\n");
+		}
+		list.add(new Board(count++,title,name,now.getTime(),sb.toString(),0));
+	}
+
+	public void displayAll() {
+		System.out.println(list);
+	}
+	
+	public void displayBoard() {
+		System.out.print("조회할 글번호 : ");
+		int snum = sc.nextInt();
+		System.out.println(list.get(snum));
+		list.get(snum).setReadCount(list.get(snum).getReadCount()+1);
+	}
+	
+	public void modifyTitle() {
+		System.out.println("수정할 글번호 : ");
+		int snum = sc.nextInt();
+		System.out.println(list.get(snum));
+		System.out.print("\n변경할 제목 : ");
+		list.get(snum).setBoardTitle(sc.nextLine());
+		System.out.println("수정되었습니다.");
+	}
+
+	public void modifyContent() {
+		System.out.println("수정할 글번호 : ");
+		int snum = sc.nextInt();
+		System.out.println(list.get(snum));
+		StringBuilder sb = new StringBuilder(list.get(snum).getBoardContent());
+		System.out.print("\n변경할 내용 : ");
+		String line = null;
+		while(!(line = sc.nextLine()).equals("exit")) {
+			sb.append(line).append("\n");
+		}
+		list.get(snum).setBoardContent(sb.toString());
+	}
+
+	public void deleteBoard() {
+		System.out.println("삭제할 글 번호 : ");
+		int snum = sc.nextInt();
+		System.out.println(list.get(snum));
+		System.out.println("정말로 삭제하시겠습니까? (y/n) : ");
+		if(sc.nextLine().toUpperCase().charAt(0) == 'Y') {
+			
+		}
+	}
+}
---
+++ src/rewind/BoardMenu.java
@@ -0,0 +1,78 @@
+package rewind;
+
+import java.util.Scanner;
+
+import board.controller.BoardManager;
+
+public class BoardMenu {
+	// Field
+	private Scanner sc = new Scanner(System.in);
+	private	BoardManager bm = new BoardManager();
+	// Constructor
+	public BoardMenu() {
+		
+	}
+	
+	public void mainMenu() {
+		int mnum;
+		do {
+			
+		System.out.print("	******* 게시글 서비스 프로그램 *******\r\n" + 
+				"\r\n" + 
+				"	1. 게시글 쓰기		\r\n" + 
+				"	2. 게시글 전체 보기		\r\n" + 
+				"	3. 게시글 한개 보기		\r\n" + 
+				"	4. 게시글 제목 수정		\r\n" + 
+				"	5. 게시글 내용 수정		\r\n" + 
+				"	6. 게시글 삭제		\r\n" + 
+				"	7. 게시글 검색		\r\n" + 
+				"	8. 파일에 저장하기	\r\n" + 
+				"	9. 정렬하기	\r\n" + 
+				"	9. 끝내기		\n" + 
+				"\r\n" + 
+				"	메뉴 번호 선택 : ");
+				mnum = sc.nextInt();
+				
+			switch(mnum) {
+			case 1 : bm.writeBoard(); break;
+			case 2 : bm.displayAllList(); break;
+			case 3 : bm.displayBoard(); break;
+			case 4 : bm.modifyTitle(); break;
+			case 5 : bm.modifyContent(); break;
+			case 6 : bm.deleteBoard(); break;
+			case 7 : bm.searchBoard(); break;
+			case 8 : bm.saveListFile(); break;
+			case 9 : sortSubMenu(); break;
+			case 0 : System.out.println("프로그램을 종료합니다. ");return;
+			}
+			
+		}while(true);
+	}
+
+	public void sortSubMenu() {
+		int mnum;
+		do {
+			System.out.print("	****** 게시글 정렬 메뉴 ******\r\n" + 
+					"\r\n" + 
+					"	1. 글번호순 오름차순정렬\r\n" + 
+					"	2. 글번호순 내림차순정렬\r\n" + 
+					"	3. 작성날짜순 오름차순정렬	\r\n" + 
+					"	4. 작성날짜순 내림차순정렬\r\n" + 
+					"	5. 글제목순 오름차순정렬\r\n" + 
+					"	6. 글제목순 내림차순정렬\r\n" + 
+					"	7. 이전 메뉴로 이동	\r\n" + 
+					"\r\n" + 
+					"	메뉴 번호 : ");
+				mnum = sc.nextInt();
+			switch(mnum) {
+			case 1 : bm.sortList(1, false); break;
+			case 2 : bm.sortList(1, true); break;
+			case 3 : bm.sortList(2, false); break;
+			case 4 : bm.sortList(2, true); break;
+			case 5 : bm.sortList(3, false); break;
+			case 6 : bm.sortList(3, true); break;
+			case 7 : return;
+			}
+		}while(true);
+	}
+}
